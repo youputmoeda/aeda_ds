bdd82e48ebe3c8a6cb753d5e89d3a355c2e37696
Maria Sottomayor <30004864@students.ual.pt>
2020-05-02 19:56:19 +0100

    updated

--- exceptions.py
+++ exceptions.py
@@ -1,8 +1,10 @@
 class EmptyListException(Exception):
     print('The list is empty')
 
+
 class InvalidPositionException(Exception):
     print('Invalid position')
+
 
 class NoSuchElementException(Exception):
     print('There is no such element')
