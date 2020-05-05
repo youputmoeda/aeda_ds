a94c54fd2f910b9f2e0e19cd715fa9beda16424e
jaascenso <joao_a_ascenso@hotmail.com>
2020-05-05 23:07:28 +0100

    Restrusture project

--- lists/exceptions.py
+++
@@ -1,9 +0,0 @@
-class EmptyListException(Exception):
-    print("Não existe elementos na lista.")
-
-class InvalidPositionException(Exception):
-    print("Posição invalida")
-
-class NoSuchElementException(Exception):
-    print("Não existe o elemento pedido")
-    
