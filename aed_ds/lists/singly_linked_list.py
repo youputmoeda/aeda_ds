3c6d918b4b6f89678cd69a6078ff4458e4f1e1fa
Maria Sottomayor <30004864@students.ual.pt>
2020-05-13 00:04:31 +0100

    No ERRORS, No FAILURES

--- aed_ds/lists/singly_linked_list.py
+++ aed_ds/lists/singly_linked_list.py
@@ -2,165 +2,153 @@ from .tad_list import List
 from .nodes import SingleListNode
 from ..exceptions import EmptyListException, InvalidPositionException
 
-class SinglyLinkedList(List):
-    
+class SinglyLinkedList(list):
+
     def __init__(self):
         self.head = None
         self.tail = None
         self.num_elements = 0
 
-    # Returns true iff the list contains no elements.
-    def is_empty(self):                                             # O(1)
+    def is_empty(self):
         return self.num_elements == 0
 
-    # Returns the number of elements in the list.
-    def size(self):                                                 # O(1)
+    def size(self):
         return self.num_elements
 
-    # Returns the first element of the list.
-    # Throws EmptyListException.
-    def get_first(self):                                            # O(1)    
+    def get_first(self): 
+        if not self.head:
+            raise EmptyListException
+        return self.head.get_element()
+
+    def get_last(self): 
+        if not self.head:
+            raise EmptyListException
+        return self.tail.get_element()
+
+    def get(self, position): 
         if not self.head:
             raise EmptyListException()
-        else:
-            return self.head.get_element()
-
-    # Returns the last element of the list.
-    # Throws EmptyListException.
-    def get_last(self):                                             # O(1)
-        if not self.head:
-            raise EmptyListException()
-        else:
-            return self.tail.get_element()
-
-    # Returns the element at the specified position in the list.
-    # Range of valid positions: 0, ..., size()-1.
-    def get(self, position):                                        # O(n)
-        index = 0
         cur_node = self.head
-        if position < 0 or position > self.size():
-            raise InvalidPositionException()
-        elif not self.head:
-            raise EmptyListException()
-        else:
-            while position > index:
-                cur_node = cur_node.next_node
-                index += 1
-            return cur_node.get_element()
+        idx = 0
+        while cur_node:
+            if idx == position:
+                return cur_node.get_element()
+            cur_node = cur_node.get_next()
+            idx += 1
 
-    # Returns the position in the list of the
-    # first occurrence of the specified element,
-    # or -1 if the specified element does not
-    # occur in the list.
-    def find(self, element):                                        # O(n)
+    def find(self, element): 
         cur_node = self.head
-        index = 0
+        idx = 0
         while cur_node:
             if element == cur_node.get_element():
-                return index
-            cur_node = cur_node.next_node
-            index += 1
-        return -1
+                return idx
+            cur_node = cur_node.get_next()
+            idx += 1
+        return -1            
 
-    # Inserts the specified element at the first position in the list.
-    def insert_first(self, element):                                # O(1)
+    def insert_first(self, element):
         new_node = SingleListNode(element, self.head)
-        # self.head = self.head.next_node
-        if not self.head: # ou seja , se a lista estiver vazia
+        if not self.head:
             self.tail = new_node
         self.head = new_node
         self.num_elements += 1
-
-    # Inserts the specified element at the last position in the list.
-    def insert_last(self, element):                                 # O(1)
-        new_node = SingleListNode(element, None)            # vai me dar o address que eu quero
-        if not self.head:                                   # Se a lista estiver vazia, o self.head = elemento
+    
+    def insert_last(self, element):
+        new_node = SingleListNode(element, None)
+        if not self.head: 
             self.head = new_node
-        else:
-            self.tail.next_node = new_node                       # cria-se a posição a seguir ao tail.  
+        else:    
+            self.tail.set_next(new_node)
         self.tail = new_node
         self.num_elements += 1
 
-    # Inserts the specified element at the specified position in the list.
-    # Range of valid positions: 0, ..., size().
-    # If the specified position is 0, insert corresponds to insertFirst.
-    # If the specified position is size(), insert corresponds to insertLast.
-    # Throws InvalidPositionException.
     def insert(self, element, position): 
-        if position < 0 or position > self.size() + 1:
+        if position < 0 or position > self.size():
             raise InvalidPositionException()
         elif position == 0:
-            self.insert_first(element)
-        elif position == self.num_elements:
-            self.insert_last(element)
-        else:
-            # get adress do next_node_node
-            new_node = self.head
-            index = 0
-            while new_node:
-                index += 1 
-                if index == position:
-                    new_node.next_node = SingleListNode(element, new_node.next_node)
-                    self.num_elements += 1
-                    break
+            return self.insert_first(element)
+        elif position == self.size():
+            return self.insert_last(element)
+        prev_node = self.head
+        cur_node = self.head
+        idx = 0
+        while prev_node:
+            if position == idx: 
+                new_node = SingleListNode(element, cur_node)
+                prev_node.set_next(new_node)
+                self.num_elements += 1
+                break
+            prev_node = cur_node
+            cur_node = cur_node.get_next()
+            idx += 1 
 
-    # Removes and returns the element at the first position in the list.
-    # Throws EmptyListException.
-    def remove_first(self):                                         # O(1)   
+    def remove_first(self):
         if not self.head:
             raise EmptyListException()
+        elif self.head == self.tail:
+            self.make_empty()
         else:
             old_head = self.head
-            self.head = self.head.next_node
-        self.num_elements -= 1
-        return old_head.get_element()
+            self.head = self.head.get_next()
+            self.num_elements -= 1
+            return old_head.get_element()
 
-    # Removes and returns the element at the last position in the list.
-    # Throws EmptyListException.
-    def remove_last(self):                                          # O(n)
-        node_second_last = self.head        
+    def remove_last(self): 
         if not self.head:
             raise EmptyListException()
-        elif not node_second_last.next_node:
-            self.remove_first()
-        else:
-            while node_second_last.next_node.next_node:
-                node_second_last = node_second_last.next_node
-            old_tail = self.tail
-            self.tail = node_second_last
-            self.tail.set_next(None)
+        elif self.size() == 1:
+            return self.remove_first()
+        elif self.size() == 2:
+            self.head.set_next(None)
+            old_node = self.head
+            self.tail = self.head
             self.num_elements -= 1
-            return old_tail.get_element()
+            return old_node.get_element() 
+        cur_node = self.head
+        while cur_node.get_next().get_next() is not None:
+            cur_node = cur_node.get_next()
+        old_node = self.tail
+        self.tail = cur_node
+        cur_node.set_next(None)
+        self.num_elements -= 1
+        return old_node.get_element()
 
-    # Removes and returns the element at the specified position in the list.
-    # Range of valid positions: 0, ..., size()-1.
-    # Throws InvalidPositionException.
     def remove(self, position): 
-        if position < 0 or position > self.size() or self.num_elements == 0:
+        if position < 0 or position > self.size() - 1:
             raise InvalidPositionException()
         elif position == 0:
-            self.remove_first()
-        elif position == self.num_elements:
-            self.remove_last()
-        else:
-            node_to_remove = self.head
-            previous_node = None
-            index = 0
-            while node_to_remove:
-                if index == position-1:
-                    previous_node = node_to_remove
-                    node_to_remove = node_to_remove.next_node
-                    old_node = node_to_remove
-                    previous_node.set_next(node_to_remove.next_node)
-                    return old_node.get_element()
-                node_to_remove = node_to_remove.next_node
-                index += 1            
+            return self.remove_first()
+        elif position == self.size() - 1:
+            return self.remove_last()
+        cur_node = self.head
+        idx = 0
+        while cur_node:
+            if idx == position:
+                prev_node.set_next(cur_node.get_next())
+                old_node = cur_node
+                cur_node.set_next(None)
+                self.num_elements -= 1
+                return old_node.get_element()
+            prev_node = cur_node
+            cur_node = cur_node.get_next()
+            idx += 1
 
-    # Removes all elements from the list.
-    def make_empty(self):                                           # O(1)
+    def make_empty(self):
         self.head = None
         self.tail = None
-        self.num_elements = 0
+        self.num_elements = 0    
 
-    # Returns an iterator of the elements in the list (in proper sequence).
     def iterator(self): pass
+
+    def print_it(self):
+        cur_node =self.head
+        if self.head:
+            print(f'Head: {self.head.get_element()}')
+            print(f'Tail: {self.tail.get_element()}')
+        else:
+            print('List is empty!')
+        while cur_node:
+            print(cur_node)
+            print(cur_node.get_element())
+            cur_node = cur_node.get_next()
+            
