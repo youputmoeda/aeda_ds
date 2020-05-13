7f5e2ee1cca69543d660a7e531a85e92eaceaeb0
André Sabino <705800+amgs@users.noreply.github.com>
2020-05-13 11:19:43 +0100

    Add singly linked list iterator has_next test.

--- tests/lists/test_singly_linked_list_iterator.py
+++ tests/lists/test_singly_linked_list_iterator.py
@@ -1,14 +1,30 @@
 import unittest
 
+from aed_ds.lists.singly_linked_list import SinglyLinkedList
+from aed_ds.lists.tad_list import List
+
 class TestSinglyLinkedListIterator(unittest.TestCase):
     def setUp(self):
         pass
 
     def test_has_next(self):
-        pass
+        singly_linked_list = SinglyLinkedList()
+        it = singly_linked_list.iterator()
+        self.assertFalse(it.has_next())
+        singly_linked_list.insert_last("element 1")
+        singly_linked_list.insert_last("element 2")
+        singly_linked_list.insert_last("element 3")
+        it = singly_linked_list.iterator()
+        self.assertTrue(it.has_next())
+        it.next()
+        self.assertTrue(it.has_next())
+        it.next()
+        self.assertTrue(it.has_next())
+        it.next()
+        self.assertFalse(it.has_next())
 
-    def test_next(self):
-        pass
+    # def test_next(self):
+    #     pass
 
-    def test_rewind(self):
-        pass
+    # def test_rewind(self):
+    #     pass
