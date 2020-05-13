21e94e49e5296e60b37d8187866fe61bf1b439a3
joao magalhaes <joao@git.com>
2020-05-13 12:36:13 +0100

    Refactor test

--- aed_ds/lists/singly_linked_list.py
+++ aed_ds/lists/singly_linked_list.py
@@ -1,10 +1,6 @@
 from .tad_list import List
 from .nodes import SinglyListNode
 from .singly_linked_list_iterator import SinglyLinkedListIterator
-
-import sys
-sys.path.append('..')
-
 from ..exceptions import *
 
 
@@ -17,9 +13,7 @@ class SinglyLinkedList(List):
    
     # Returns true if the list contains no elements.
     def is_empty(self):
-        if self.head is None:
-            return True
-        return False        
+        return self.num_elements == 0
 
     # Returns the number of elements in the list.
     def size(self):
@@ -27,62 +21,54 @@ class SinglyLinkedList(List):
 
     # Returns the first element of the list.
     # Throws EmptyListException.
-    def get_first(self):
-        try:    
-            if not self.head:
-                raise Exception
-            else:
-                return self.head.get_element()
-        except:
+    def get_first(self):  
+        if not self.head:
             raise EmptyListException()
+        return self.head.get_element()
 
     # Returns the last element of the list.
     # Throws EmptyListException.
     def get_last(self):
-        try:
-            if not self.tail:
-                raise Exception
-            else:
-                return self.tail.get_element()
-        except:
+        if not self.tail:
             raise EmptyListException()
+        return self.tail.get_element()
     
     # Returns the element at the specified position in the list.
     # Range of valid positions: 0, ..., size()-1.
     def get(self, position):
-        aux = 0
-        node_to_iterate = self.head
-        try:
-            if position < 0 or position > self.size():
-                raise Exception
-            while aux < position:
-                node_to_iterate = node_to_iterate.next_node
-                aux += 1
-            return node_to_iterate.get_element()
-        except:
+        if position < 0 or position > self.size():
             raise InvalidPositionException()
+        if not self.head:
+            raise EmptyListException()
+        cur_node = self.head
+        idx = 0
+        while cur_node:
+            if idx == position:
+                return cur_node.get_element()
+            cur_node = cur_node.get_next()
+            idx += 1
+        return cur_node.get_element()
 
     # Returns the position in the list of the
     # first occurrence of the specified element,
     # or -1 if the specified element does not
     # occur in the list.
     def find(self, element):
-        position = 0
-        node_to_iterate = self.head
-        while node_to_iterate is not None:
-            if node_to_iterate.get_element() == element:
-                return position
-            else:
-                node_to_iterate = node_to_iterate.next_node
-                position += 1
+        cur_node = self.head
+        idx = 0
+        while cur_node:
+            if element == cur_node.get_element():
+                return idx
+            cur_node = cur_node.get_next()
+            idx += 1
         return -1
               
     # Inserts the specified element at the first position in the list.
     def insert_first(self, element):                            # O(1)
         new_node = SinglyListNode(element, self.head)
-        self.head = new_node  
         if self.tail is None:
             self.tail = new_node
+        self.head = new_node  
         self.num_elements += 1 
 
     # Inserts the specified element at the last position in the list.
@@ -91,7 +77,7 @@ class SinglyLinkedList(List):
         if not self.head:                                   # Se a lista estiver vazia, o self.head = elemento
             self.head = new_node
         else: 
-            self.tail.next_node = new_node                       # cria-se a posição a seguir ao tail.  
+            self.tail.set_next(new_node)               # cria-se a posição a seguir ao tail
         self.tail = new_node
         self.num_elements += 1
 
@@ -101,78 +87,84 @@ class SinglyLinkedList(List):
     # If the specified position is size(), insert corresponds to insertLast.
     # Throws InvalidPositionException.
     def insert(self, element, position):
-        try:
-            if position < 0 or position > self.size() + 1:
-                raise Exception
-            elif position == 0:
-                self.insert_first(element)
-            elif position == self.num_elements:
-                self.insert_last(element)
-            else:
-                #get adress do next_node_node
-                new_node = self.head
-                index = 0
-                while new_node:
-                    index += 1 
-                    if index == position:
-                        new_node.next_node = SinglyListNode(element, new_node.next_node)
-                        self.num_elements += 1
-                        break
-        except:
-            InvalidPositionException              
+        if position < 0 or position > self.size():
+            raise InvalidPositionException()
+        elif position == 0:
+            return self.insert_first(element)
+        elif position == self.size:
+            return self.insert_last(element)
+        prev_node = self.head
+        cur_node = self.head
+        idx = 0
+        while prev_node:
+            if position == idx: 
+                new_node = SingleListNode(element, cur_node)
+                prev_node.set_next(new_node)
+                self.num_elements += 1
+                break
+            prev_node = cur_node
+            cur_node = cur_node.get_next()
+            idx += 1
+           
 
     # Removes and returns the element at the first position in the list.
     # Throws EmptyListException.
     def remove_first(self):
         if not self.head:
             raise EmptyListException()
+        elif self.head == self.tail:
+            self.make_empty
         else:
             node_to_remove = self.head
-            self.head = node_to_remove.next_node
-        self.num_elements -= 1
-        return node_to_remove.get_element()
+            self.head = node_to_remove.get_next()
+            self.num_elements -= 1
+            return node_to_remove.get_element()
 
     # Removes and returns the element at the last position in the list.
     # Throws EmptyListException.
     def remove_last(self):
         if not self.head:
             raise EmptyListException()
-        else:
-            # find second last node e fazer com que esse node se torne no último elemento da minha lista
-            node_second_last = self.head
-            while node_second_last.next_node.next_node is not None:
-                node_second_last = node_second_last.next_node
-            old_tail = self.tail
-            self.tail = node_second_last
-            self.tail.set_next(None)
+        elif self.head == self.tail:
+            return self.remove_first()
+        elif self.size() == 2:
+            self.head.set_next(None)
+            old_none = self.head
+            self.tail = self.head
             self.num_elements -= 1
-            return old_tail.get_element()
+            return old_none.get_element()
+            # find second last node e fazer com que esse node se torne no último elemento da minha lista
+        cur_node = self.head
+        while cur_node.get_next().get_next() is not None:
+            cur_node = cur_node.get_next()
+        old_none = self.tail
+        self.tail = cur_node
+        self.tail.set_next(None)
+        self.num_elements -= 1
+        return old_none.get_element()
 
     # Removes and returns the element at the specified position in the list.
     # Range of valid positions: 0, ..., size()-1.
     # Throws InvalidPositionException.
     def remove(self, position):
-        number = 0
-        if self.is_empty():
-            raise EmptyListException()
-        try:
-            if position < 0 or position>=self.size():
-                raise Exception
-            else:
-                node_to_iterate = self.head
-                previous_node = None
-                while node_to_iterate:
-                    if number == position:
-                        node_to_remove = node_to_iterate
-                        previous_node.set_next(node_to_remove.next_node)
-                        return node_to_remove.get_element()
-                        self.number_elements -= 1
-                    else:
-                        previous_node = node_to_iterate
-                        node_to_iterate = node_to_iterate.next_node
-                        number += 1
-        except:
+        if position < 0 or position>=self.size():
             raise InvalidPositionException()
+        elif position == 0:
+            return self.remove_first()
+        elif position == self.size() -= 1:
+            return self.remove_last
+        cur_node = self.head
+        idx = 0
+        while cur_node:
+            if idx == position:
+                prev_node.set_next(cur_node.get_next())
+                old_node = cur_node
+                cur_node.set_next(None)
+                self.number_elements -= 1
+                return node_to_remove.get_element()       
+            prev_node = cur_node
+            cur_node = cur_node.get_next()
+            idx += 1
                         
  
     # # Removes all elements from the list.
@@ -183,9 +175,7 @@ class SinglyLinkedList(List):
 
     # Returns an iterator of the elements in the list (in proper sequence).
     def iterator(self):
-        iterator = SinglyLinkedListIterator(self.head)
-        for _ in range(self.size()):
-            return iterator.next()
+        return SinglyLinkedListIterator(self)
 
 
 
@@ -193,29 +183,30 @@ class SinglyLinkedList(List):
         current_node = self.head
         while current_node:
             print(current_node.element)
-            current_node = current_node.next_node
+            current_node = current_node.get_next()
             
 # criar a lista:
-#llist = singly_linked_list()
+llist = SinglyLinkedList()
 #print(llist.get_first())
 # inserir elementos no inicio da lista:
-#llist.insert_first('C')
-#llist.insert_first('A')
+llist.insert_first('C')
+llist.insert_first('A')
 # inserir elementos no fim da lista:
-# llist.insert_last('D')
+llist.insert_last('D')
 # inserir elementos numa posição expecífica da lista:
 # llist.insert('B', 1)
 # llist.insert('E', 4)
 #print(f'position: {1}, element: {llist.get(45)}')
 #llist.insert('W', 9)
 # imprimir a lista
-# llist.print_list()
+llist.print_list()
 # print('=' * 30)
 # check size:
 # print(llist.size())
 # print('=' * 30)
 # iterator
-# print(llist.iterator())
+print(llist.iterator())
+print(llist.iterator())
 # print('=' * 30)
 # get the first element of the list:
 # print(llist.get_first())
--- aed_ds/lists/singly_linked_list_iterator.py
+++ aed_ds/lists/singly_linked_list_iterator.py
@@ -1,41 +1,30 @@
-import sys
-sys.path.append('..')
 from ..exceptions import *
 from ..tad_iterator import Iterator
 
 class SinglyLinkedListIterator(Iterator):
     
-    def __init__(self, maxi):
-        self.maxi = maxi
-        self.first = maxi
+    def __init__(self, singly_linked_list):
+        self.singly_linked_list = singly_linked_list
+        self.rewind()
 
 
     # Returns true iff the iteration has more elements.
-    # In other words, returns true next would return an element rather than throwing an exception.
     def has_next(self):
-        if self.maxi.get_next() != None:
-            return True
-        return False
-    
-    def __iter__(self):
-        return self
+        return self.current_node != None
 
     # Returns the next element in the iteration.
     # Throws NoSuchElementException
     def next(self):
-        try:    
-            if self.has_next() is False:
-                raise Exception
-            else: 
-                elem = self.maxi.get_element()
-                self.maxi.get_next()
-                return elem
-        except:
-            NoSuchElementException()
-            
+        if not self.has_next():
+            raise NoSuchElementException() 
+        element = self.current_node.get_element()
+        self.current_node = self.current_node.get_next()
+        return element
 
     # Restarts the iteration. After rewind, if the iteration is not empty, next will return the first element in the iteration.
     def rewind(self):
-        self.maxi = self.first
-        self.next()
+        self.current_node = self.singly_linked_list.head
     
+ 
+
+            
