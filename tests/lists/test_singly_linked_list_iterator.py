d77003791f0dca4644c34c9ce9a0779058ee7d03
André Sabino <705800+amgs@users.noreply.github.com>
2020-05-13 11:33:45 +0100

    Passing all singly linked list iterator tests.

--- tests/lists/test_singly_linked_list_iterator.py
+++ tests/lists/test_singly_linked_list_iterator.py
@@ -2,6 +2,7 @@ import unittest
 
 from aed_ds.lists.singly_linked_list import SinglyLinkedList
 from aed_ds.lists.tad_list import List
+from aed_ds.exceptions import NoSuchElementException
 
 class TestSinglyLinkedListIterator(unittest.TestCase):
     def setUp(self):
@@ -23,8 +24,28 @@ class TestSinglyLinkedListIterator(unittest.TestCase):
         it.next()
         self.assertFalse(it.has_next())
 
-    # def test_next(self):
-    #     pass
+    def test_next(self):
+        singly_linked_list = SinglyLinkedList()
+        it = singly_linked_list.iterator()
+        with self.assertRaises(NoSuchElementException):
+            it.next()
+        singly_linked_list.insert_last("element 1")
+        singly_linked_list.insert_last("element 2")
+        singly_linked_list.insert_last("element 3")
+        it = singly_linked_list.iterator()
+        self.assertEqual(it.next(), "element 1")
+        self.assertEqual(it.next(), "element 2")
+        self.assertEqual(it.next(), "element 3")
+        with self.assertRaises(NoSuchElementException):
+            it.next()
 
-    # def test_rewind(self):
-    #     pass
+    def test_rewind(self):
+        singly_linked_list = SinglyLinkedList()
+        singly_linked_list.insert_last("element 1")
+        singly_linked_list.insert_last("element 2")
+        singly_linked_list.insert_last("element 3")
+        it = singly_linked_list.iterator()
+        self.assertEqual(it.next(), "element 1")
+        self.assertEqual(it.next(), "element 2")
+        it.rewind()
+        self.assertEqual(it.next(), "element 1")
