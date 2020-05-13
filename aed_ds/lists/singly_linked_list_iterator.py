8077fa5f41de4f699063b399a14ea337fd7b3858
André Sabino <705800+amgs@users.noreply.github.com>
2020-05-13 10:53:54 +0100

    Add singly linked list iterator without tests.

--- aed_ds/lists/singly_linked_list_iterator.py
+++ aed_ds/lists/singly_linked_list_iterator.py
@@ -1,31 +1,24 @@
-from ..exception import *
+from ..exceptions import *
 from ..tad_iterator import Iterator
 
 class SinglyLinkedListIterator(Iterator):
-    
-    def __init__(self):
-        self.element = element
+    def __init__(self, singly_linked_list):
+        self.singly_linked_list = singly_linked_list
+        self.rewind()
 
     # Returns true iff the iteration has more elements.
-    # In other words, returns true next would return an element rather than throwing an exception.
-    def has_next(self): 
-        return self.element != None
+    def has_next(self):
+        return self.current_node != None
 
     # Returns the next element in the iteration.
     # Throws NoSuchElementException
-    def next(self): pass
+    def next(self):
+        if not self.has_next():
+            raise NoSuchElementException()
+        element = self.current_node.get_element()
+        self.current_node = self.current_node.get_next()
+        return element
 
     # Restarts the iteration. After rewind, if the iteration is not empty, next will return the first element in the iteration.
-    def rewind(self): pass
-
-class TwoWayIterator(Iterator):
-    # Returns true iff the iteration has more elements in the reverse direction.
-    # In other words, returns true if previous would return an element rather than throwing an exception.
-    def has_previous(self): pass
-    
-    # Returns the previous element in the iteration.
-    # Throws NoSuchElementException
-    def previous(self): pass
-
-    # Restarts the iteration in the reverse direction. After fullForward, if the iteration is not empty, previous will return the last element in the iteration.
-    def full_forward(self): pass
+    def rewind(self):
+        self.current_node = self.singly_linked_list.head
